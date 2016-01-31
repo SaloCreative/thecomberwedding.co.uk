5c694fc9ad3a5fee120ae80eda84daa6a2378469
Rich Comber <rich@elementalwebdesign.co.uk>
2016-01-31 16:53:57 +0000

    Styling updates

--- public_html/app/themes/comber-wedding/page-templates/home.php
+++ public_html/app/themes/comber-wedding/page-templates/home.php
@@ -6,8 +6,14 @@
  * @subpackage comberwedding
  * @since 1.0
  */
-?>
-<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
+
+$background = '';
+if (has_post_thumbnail( $post->ID ) ):
+    $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0];
+    $background = 'style="background-image: url('.$image.')"';
+endif;
+
+Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
     <section id="primary">
         <div class="large-12 columns">
--- public_html/app/themes/comber-wedding/parts/shared/header.php
+++ public_html/app/themes/comber-wedding/parts/shared/header.php
@@ -1,8 +1,14 @@
 <?php if(!is_user_logged_in()) { header( 'Location: '.home_url('/login') ); die(); } ?>
 <div id="body-wrapper" class="row">
-    <div id="masthead">
-        <div class="large-6 medium-6 columns">
-            <a class="logo" href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a>
-            <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/nav') ); ?>
+    <div id="masthead" data-equalizer>
+        <div class="medium-6 columns intro-image">
+            <div class="panel" data-equalizer-watch style="background-image: url(<?= $background; ?>);">
+            </div>
+        </div>
+        <div class="medium-6 columns">
+            <div class="panel" data-equalizer-watch>
+                <a class="logo" href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a>
+                <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/nav') ); ?>
+            </div>
         </div>
     </div>
