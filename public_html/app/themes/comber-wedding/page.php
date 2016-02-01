f0345d4c0af859d4205b12fbb92956d0d2ac2c58
Rich Comber <rich@elementalwebdesign.co.uk>
2016-02-01 21:32:51 +0000

    Adding photo column to default page template

--- public_html/app/themes/comber-wedding/page.php
+++ public_html/app/themes/comber-wedding/page.php
@@ -1,30 +1,36 @@
 <?php
 /**
- * The template for displaying all pages.
+ * Template Name: Home Page
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * Please see /external/starkers-utilities.php for info on Starkers_Utilities::get_template_parts()
- *
- * @package 	WordPress
- * @subpackage 	EWD Foundation Theme
- * @since 		1.0
+ * @package WordPress
+ * @subpackage comberwedding
+ * @since 1.0
  */
 
+$background = '';
+if (has_post_thumbnail( $post->ID ) ):
+	$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0];
+	$background = 'style="background-image: url('.$image.')"';
+endif;
+
 Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
-<div id="primary" class="row" data-equalizer>
-	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-	<div class="large-9 columns">
-		<div class="panel">
-			<h1><?php the_title(); ?></h1>
-			<?php the_content(); ?>
+
+	<section id="primary">
+		<div class="large-6 columns">
+			<?php
+			if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+				<div class="panel">
+					<?php the_content(); ?>
+				</div>
+			<?php endwhile; ?>
 		</div>
-	</div>
-	<?php endwhile; ?>
-	<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/sidebar' ) ); ?>
-	
-</div>
+		<div class="large-6 columns">
+			<div class="panel">
+				Sidebar for photos
+			</div>
+		</div>
+	</section>
+
+
+
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
