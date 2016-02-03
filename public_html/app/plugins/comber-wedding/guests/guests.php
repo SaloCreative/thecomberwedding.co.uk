6ab78544bea0d651d753549e2ce954adf1c05cb6
Rich Comber <rich@elementalwebdesign.co.uk>
2016-02-03 22:15:54 +0000

    Storing group id for getting guest results

--- public_html/app/plugins/comber-wedding/guests/guests.php
+++ public_html/app/plugins/comber-wedding/guests/guests.php
@@ -4,11 +4,12 @@ require_once('app/plugins/comber-wedding/database/return.php');
 $user_ID = get_current_user_id();
 
 try {
-    $data = $ftoDB->query('SELECT * FROM guests_group WHERE id ='.$user_ID);
+    $data = $ftoDB->query('SELECT * FROM guests_group WHERE user_id ='.$user_ID);
     //Check there are orders
     if ($data->rowCount() > 0) {
-        foreach($data as $guest) {
-            $groupName = $guest['name'];
+        foreach($data as $guestGroup) {
+            $groupName = $guestGroup['name'];
+            $groupID = $guestGroup['id'];
         }
     } else {
         $groupName =  'Not Found';
