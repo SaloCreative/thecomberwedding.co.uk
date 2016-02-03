c81767c12ec47f7386d3a2fad41f70c08b7ec517
Rich Comber <rich@elementalwebdesign.co.uk>
2016-02-03 21:50:07 +0000

    Updating footer

--- public_html/app/themes/comber-wedding/parts/shared/footer.php
+++ public_html/app/themes/comber-wedding/parts/shared/footer.php
@@ -1,8 +1,6 @@
 	<footer>
-		<div class="columns large-12">
-			<div class="panel">
+		<div class="columns large-12 text-center">
 				<p>&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
-			</div>
 		</div>
 	</footer>
 </div>
