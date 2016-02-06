a4701aaf427249697e4dda35d023db6d6e933e70
Rich Comber <rich@elementalwebdesign.co.uk>
2016-02-06 12:47:50 +0000

    Setting up RSVP save action

--- public_html/app/plugins/comber-wedding/guests/rsvp-save.php
+++ public_html/app/plugins/comber-wedding/guests/rsvp-save.php
@@ -7,6 +7,24 @@ if (!empty($_POST['rsvpNonce']) && !empty($_POST['userId']) && !empty($_POST['us
     $wpUser = $_POST['userId'];
     $userEmail = $_POST['userEmail'];
     require_once('../database/identifyGuest.php');
+    foreach($_POST['guestId'] as $index => $null) {
+        $guest = $_POST['guestId'][$index];
+        $rsvp = $_POST['rsvp'][$index];
+        $starter = $_POST['starter'][$index];
+        $main = $_POST['main'][$index];
+        $dessert = $_POST['dessert'][$index];
+        echo $guest.'<br>'.$rsvp.'<br>'.$starter.'<br>'.$main.'<br>'.$dessert.'<br><br>';
+        // TODO: Ensure this user can respond for this guest
+        try {
+            $rsvpInsert = 'UPDATE guests SET rsvp = '.$rsvp.', starter = '.$starter.', main = '.$main.', dessert = '.$dessert.' WHERE id = '.$guest;
+            $insertRSVP = $ftoDB->prepare($rsvpInsert);
+            $insertRSVP->execute();
+        }
+        catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            $ftoDB = null;
+        }
+    }
 }
 else {
     header( 'Location: /rsvp?alert=true&failed=identify');
