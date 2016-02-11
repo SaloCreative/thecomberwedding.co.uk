a59392dc35cb16d1e5afe2870de4ea8a8e8419ad
Rich Comber <rich@elementalwebdesign.co.uk>
2016-02-11 19:03:04 +0000

    rejigigng admin templates

--- public_html/app/themes/comber-wedding/page-templates/admin.php
+++ public_html/app/themes/comber-wedding/page-templates/admin.php
@@ -7,16 +7,23 @@
  * @since 1.0
  */
 if (!isSiteAdmin() ){ header( 'Location: '.home_url() ); die(); }
-Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
+Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header-admin' ) ); ?>
 
     <section id="primary">
-        <div class="large-12 columns">
+        <div class="medium-8 columns">
             <?php
             if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
                 <div class="panel">
                     <?php the_content(); ?>
                 </div>
             <?php endwhile; ?>
+        </div>
+        <div class="medium-4 columns">
+            <div class="panel">
+               <h3>Admin area</h3>
+                <hr>
+                <?= do_shortcode('[invite_status]'); ?>
+            </div>
         </div>
     </section>
 
---
+++ public_html/app/themes/comber-wedding/parts/shared/header-admin.php
@@ -0,0 +1,6 @@
+<?php if(!is_user_logged_in()) { header( 'Location: '.home_url('/login') ); die(); } ?>
+<div id="body-wrapper" class="row">
+    <div id="masthead" data-equalizer>
+        <div class="columns large-12">
+        </div>
+    </div>
