781f3fea8fea8849e416bc3a9d8838a883c603a5
Rich Comber <rich@elementalwebdesign.co.uk>
2016-02-14 15:21:57 +0000

    Setting up RSVP summary output

--- public_html/app/plugins/comber-wedding/admin/functions/rsvp_quick_summary.php
+++ public_html/app/plugins/comber-wedding/admin/functions/rsvp_quick_summary.php
@@ -4,12 +4,12 @@ function comber_rsvp_summary_build() {
     ob_start();
     require('guest_summary.php'); ?>
     <div class="container">
-        <h2>The Comber Wedding <em><?= $totalGuests ;?> invited</em></h2>
-        <ul class="tabs" data-tab>
-            <li class="tab-title active"><a href="#panel1">Attending <em>(<?= $yesCount; ?>)</em></a></li>
-            <li class="tab-title"><a href="#panel2">Not Attending <em>(<?= $noCount; ?>)</em></a></li>
-            <li class="tab-title"><a href="#panel3">Awaiting Response <em>(<?= $waitCount; ?>)</em></a></li>
-        </ul>
+        <div class="panel">
+            <h3>The Comber Wedding</h3>
+            <p><strong><?= $yesCount; ?></strong>/<?= $totalGuests ;?><br><span>Attending</span></p>
+            <p><strong><?= $noCount; ?></strong>/<?= $totalGuests ;?><br><span>Not Attending</span></p>
+            <p><strong><?= $waitCount; ?></strong>/<?= $totalGuests ;?><br><span>Awaiting Response</span></p>
+        </div>
     </div>
     <?php
     return ob_get_clean();
