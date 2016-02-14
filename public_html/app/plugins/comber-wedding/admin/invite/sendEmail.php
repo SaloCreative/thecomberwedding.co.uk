662ecb416ed8fc357c54566b5d6143fd13f5d7ac
Rich Comber <rich@elementalwebdesign.co.uk>
2016-02-14 18:02:49 +0000

    Adding basic send function to email

--- public_html/app/plugins/comber-wedding/admin/invite/sendEmail.php
+++ public_html/app/plugins/comber-wedding/admin/invite/sendEmail.php
@@ -1,13 +1,22 @@
 <?php
-ini_set('display_errors', true);
-error_reporting(E_ALL);
 
 echo 'Mailing<br>';
 
-require '../../../../../../vendor/autoload.php';
+require '../../../../../vendor/autoload.php';
 use Mailgun\Mailgun;
+
+//TODO: Centralise this to link to main mailgun plugin
 
 # Instantiate the client.
 $mgClient = new Mailgun('key-58bdcb87c0c6277d258f41492c248312');
 $domain = "mg.thecomberwedding.co.uk";
 
+# Make the call to the client.
+$result = $mgClient->sendMessage($domain, array(
+    'from'    => 'Lotte & Rich Comber <invites@thecomberwedding.co.uk>',
+    'to'      => $email,
+    'subject' => 'You are invited to our Wedding!',
+    'html'    => '<html>
+		'.$messageCompanyOutput.'
+	</html>'
+));
