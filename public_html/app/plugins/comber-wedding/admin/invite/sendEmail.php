450df1cb406be8fbd2c898388030bbd6f4192ba1
Rich Comber <rich@elementalwebdesign.co.uk>
2016-02-14 17:47:28 +0000

    Adding mailgun API key and client details

--- public_html/app/plugins/comber-wedding/admin/invite/sendEmail.php
+++ public_html/app/plugins/comber-wedding/admin/invite/sendEmail.php
@@ -4,10 +4,10 @@ error_reporting(E_ALL);
 
 echo 'Mailing<br>';
 
-/*require '../../../../../vendor/autoload.php';
+require '../../../../../../vendor/autoload.php';
 use Mailgun\Mailgun;
 
 # Instantiate the client.
 $mgClient = new Mailgun('key-58bdcb87c0c6277d258f41492c248312');
-$domain = "mg.fruit2office.co.uk";
-*/
+$domain = "mg.thecomberwedding.co.uk";
+
