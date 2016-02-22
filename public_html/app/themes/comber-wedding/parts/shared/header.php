59ef94cc5e92aeeeabcd29cf99bb43c648576885
Rich Comber <rich@elementalwebdesign.co.uk>
2016-02-22 19:24:06 +0000

    Adding proper header image

--- public_html/app/themes/comber-wedding/parts/shared/header.php
+++ public_html/app/themes/comber-wedding/parts/shared/header.php
@@ -1,6 +1,6 @@
 <?php if(!is_user_logged_in()) { header( 'Location: '.home_url('/login') ); die(); }
 $background = '';
-$featured_image ='';
+$featured_image ='<img src="'.get_stylesheet_directory_uri().'/images/main-header.jpg" />';
 if (has_post_thumbnail( $post->ID ) ):
     $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0];
     $background = 'style="background-image: url('.$image.')"';
@@ -11,6 +11,12 @@ endif;?>
         <div class="medium-8 columns intro-image">
             <div class="panel" data-equalizer-watch>
                 <?= $featured_image; ?>
+                <div class="header-wrapper">
+                    <p class="intro">You are invited to...</p>
+                    <h1>The Comber Wedding!</h1>
+                    <p class="date">Saturday 4th June 2016</p>
+                    <p class="local">2pm at The Sanbanks Hotel</p>
+                </div>
             </div>
         </div>
         <div class="medium-4 columns">
