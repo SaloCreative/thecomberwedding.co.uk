a61572e2132d3ce88bf093bc21e8f975ed7848a2
Rich Comber <rich@elementalwebdesign.co.uk>
2016-02-22 18:45:39 +0000

    Adding logiut link

--- public_html/app/plugins/comber-wedding/comber-wedding.php
+++ public_html/app/plugins/comber-wedding/comber-wedding.php
@@ -21,3 +21,4 @@ function isSiteAdmin(){
     $currentUser = wp_get_current_user();
     return in_array('administrator', $currentUser->roles);
 }
+show_admin_bar(false);
--- public_html/app/themes/comber-wedding/parts/shared/nav.php
+++ public_html/app/themes/comber-wedding/parts/shared/nav.php
@@ -1,11 +1,14 @@
 <nav id="navigation">
-	<?php
-	if ( has_nav_menu( 'primary-menu' ) ) {
-		wp_nav_menu( array(
-				'theme_location' => 'primary-menu',
-				'container'       => '',
-				'items_wrap'      => '<ul class="main-navigation">%3$s</ul>',
-				'walker' => new My_Walker_Nav_Menu()
-		) );
-	} ?>
+	<ul class="main-navigation">
+		<?php
+		if ( has_nav_menu( 'primary-menu' ) ) {
+			wp_nav_menu( array(
+					'theme_location' => 'primary-menu',
+					'container'       => '',
+					'items_wrap'      => '%3$s',
+					'walker' => new My_Walker_Nav_Menu()
+			) );
+		} ?>
+		<li class="logout"><a href="<?php echo wp_logout_url( get_home_url() ); ?>" title="Logout">Logout</a></li>
+	</ul>
 </nav>
