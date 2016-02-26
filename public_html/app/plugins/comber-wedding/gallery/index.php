f883fc17c63138bb257b89dfd82df0fd7e2050f1
Rich Comber <rich@elementalwebdesign.co.uk>
2016-02-26 22:04:29 +0000

    Moving out form until its needed

--- public_html/app/plugins/comber-wedding/gallery/index.php
+++ public_html/app/plugins/comber-wedding/gallery/index.php
@@ -1,21 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Dropzone simple example</title>
-    <script src="/js/dropzone.js"></script>
-    <script src="/js/dropzone-options.js"></script>
-    <link rel="stylesheet" href="/css/dropzone.css">
-</head>
-<body>
-<form action="/gallery/upload.php" class="dropzone" id="myWeddingGallery">
+<form action="<?= plugins_url();?>/comber-wedding/gallery/upload.php" class="dropzone" id="myWeddingGallery">
     <div class="fallback">
         <input name="file" type="file" multiple />
     </div>
+    <?php //Pass user details to save action to crudely verify
+    $current_user = wp_get_current_user();
+    $userEmail = $current_user->user_login;
+    $wpUser = $current_user->ID; ?>
+    <input type="hidden" name="userId" value="<?= $wpUser; ?>" />
+    <input type="hidden" name="userEmail" value="<?= $userEmail; ?>" />
+    <input type="hidden" name="galleryNonce" value="12r23tfwegwqt4yefew1^$@36gfu2fg239urt287rbc278vc2bvc7" />
 </form>
-
-</body>
-</html>
-
-
-
--- public_html/app/themes/comber-wedding/page-templates/dynamic-sidebar.php
+++ public_html/app/themes/comber-wedding/page-templates/dynamic-sidebar.php
@@ -21,18 +21,6 @@ Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'part
             if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
                 <div class="panel" data-equalizer-watch>
                     <?php the_content(); ?>
-                    <form action="<?= plugins_url();?>/comber-wedding/gallery/upload.php" class="dropzone" id="myWeddingGallery">
-                        <div class="fallback">
-                            <input name="file" type="file" multiple />
-                        </div>
-                        <?php //Pass user details to save action to crudely verify
-                        $current_user = wp_get_current_user();
-                        $userEmail = $current_user->user_login;
-                        $wpUser = $current_user->ID; ?>
-                        <input type="hidden" name="userId" value="<?= $wpUser; ?>" />
-                        <input type="hidden" name="userEmail" value="<?= $userEmail; ?>" />
-                        <input type="hidden" name="galleryNonce" value="12r23tfwegwqt4yefew1^$@36gfu2fg239urt287rbc278vc2bvc7" />
-                    </form>
                 </div>
             <?php endwhile; ?>
         </div>
